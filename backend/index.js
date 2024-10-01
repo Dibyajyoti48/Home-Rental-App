4d2ae5d7fb0e2711a53895f69b84290e3336e19a
Dibyajyoti48 <dibyajyotip17@gmail.com>
2024-10-01 20:54:15 +0530

    User model

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ import mongoose from "mongoose"
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose
+.connect(process.env.MONGO_URI).then(()=>{
     console.log("Connected to MongoDB");
 })
 .catch((err)=>{
