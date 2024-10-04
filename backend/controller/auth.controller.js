f9d9467b94f856d42e888d12b07074616a0191d4
Dibyajyoti48 <dibyajyotip17@gmail.com>
2024-10-04 23:53:57 +0530

    Login api route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
 
-export const register = async(req,res,next)=> {
+export const register = async(req,res,_next)=> {
     try {
         const{ firstName, lastName, email, password } = req.body
 
@@ -28,5 +29,25 @@ export const register = async(req,res,next)=> {
 
     } catch(error){
         console.log(error);
+    }
+}
+export const login = async (req, res) => {
+    try {
+        const {email,password} = req.body
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return res.status(409).json({message:"User does't exist!"})
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword){
+            return res.status(400).json({message:"Wrong credentials!"})
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const {password: pass, ...rest} = validUser._doc
+        res.status(200).json({token,rest})
+
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({error:error.message})
     }
 }
