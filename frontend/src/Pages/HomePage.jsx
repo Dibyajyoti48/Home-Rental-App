799790c4132dc83bbb316e1810158a7e52b2dbdc
Dibyajyoti48 <dibyajyotip17@gmail.com>
2024-11-10 00:20:32 +0530

    Listings(HomePage) and listing card

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Categories from '../components/Categories'
+import Listings from '../components/Listing'
 
 const Homepage = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <Categories/>
+      <Navbar />
+      <Hero />
+      <Categories />
+      <Listings />
     </div>
   )
 }
